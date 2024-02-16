77bda6af9bd485fa97d22346353b750f5682947c
dluman <dluman@allegheny.edu>
2024-02-16 09:18:52 -0500

    Renaming jupiter package

--- 5/src/test/java/com/golf/five/BaseTests.java
+++ 5/src/test/java/com/golf/five/BaseTests.java
@@ -16,7 +16,7 @@ import java.util.List;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.juputer.api.AfterEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
